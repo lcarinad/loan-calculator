3c1e36db58d99c49da475ecc8928db774b2eff59
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-05-24 10:32:36 -0400

    wrote spec 1

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,10 @@
-
-it('should calculate the monthly rate correctly', function () {
-
+it("should calculate the monthly rate correctly", function () {
+  const values = { amount: 11000, years: 4, rate: 4.7 };
+  expect(calculateMonthlyPayment(values)).toEqual(251.83);
 });
 
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
+// it("should return a result with 2 decimal places", function () {
+//   // ..
+// });
 
 /// etc
