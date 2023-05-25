66cc791b1b3caeb7e82e5ec1929766d9d14d38e5
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-05-25 14:24:35 -0400

    changed number to string

--- calculator-test.js
+++ calculator-test.js
@@ -1,6 +1,6 @@
 it("should calculate the monthly rate correctly", function () {
   const values = { amount: 11000, years: 4, rate: 4.7 };
-  expect(calculateMonthlyPayment(values)).toEqual(251.83);
+  expect(calculateMonthlyPayment(values)).toEqual("251.83");
 });
 
 // it("should return a result with 2 decimal places", function () {
