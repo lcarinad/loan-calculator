30aa61512db47955e0acde3e3352c584f521f82a
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-05-25 14:31:10 -0400

    added second test

--- calculator-test.js
+++ calculator-test.js
@@ -3,8 +3,7 @@ it("should calculate the monthly rate correctly", function () {
   expect(calculateMonthlyPayment(values)).toEqual("251.83");
 });
 
-// it("should return a result with 2 decimal places", function () {
-//   // ..
-// });
-
-/// etc
+it("should return a result with 2 decimal places", function () {
+  const values = { amount: 11000.5, years: 4, rate: 4.7 };
+  expect(calculateMonthlyPayment(values)).toEqual("251.84");
+});
